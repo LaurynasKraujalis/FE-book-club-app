90660f1fe2cbf279128ef366bf92408a7d142397
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-05-19 09:47:42 +0200

    changed api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://scifi-amsterdam.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;

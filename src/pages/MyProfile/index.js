c9121587f53d516840c6d8b8b0650193cbd660c0
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-05-21 14:05:54 +0200

    setup form for changing userInfo. Also rendering user info

--- src/pages/MyProfile/index.js
+++ src/pages/MyProfile/index.js
@@ -1,6 +1,63 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { selectUser } from "../../store/user/selectors";
+
+import {
+  Col,
+  Image,
+  InputGroup,
+  Button,
+  FormControl,
+  Container,
+  Card,
+} from "react-bootstrap";
 
 export default function MyProfile() {
-  return <Container> My profile</Container>;
+  const user = useSelector(selectUser);
+  const [profileImage, setProfileImage] = useState("");
+  const [userMotto, setUserMotto] = useState("");
+  console.log("image", profileImage);
+  console.log("motto", userMotto);
+
+  return (
+    <div>
+      <Container>
+        <Col md="auto">
+          Profile image
+          <Image src={user.image} fluid />
+        </Col>
+      </Container>
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <Button variant="danger">New image URL</Button>
+        </InputGroup.Prepend>
+        <FormControl
+          aria-describedby="basic-addon1"
+          value={profileImage}
+          onChange={(event) => setProfileImage(event.target.value)}
+        />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <Button variant="danger">Change motto</Button>
+        </InputGroup.Prepend>
+        <FormControl
+          aria-describedby="basic-addon1"
+          value={userMotto}
+          onChange={(event) => setUserMotto(event.target.value)}
+        />
+      </InputGroup>
+
+      <Container>
+        <Card bg="secondary" text="light" border="danger">
+          <Card.Subtitle style={{ color: "black" }}>Your motto</Card.Subtitle>
+          <Card.Body className="text-center">
+            {user.motto ? <Card.Text>"{user.motto}"</Card.Text> : null}
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
 }

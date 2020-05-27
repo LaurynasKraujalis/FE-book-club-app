acc1dc863bc8a64ea6527a3a0ff60165e2347f21
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-05-27 15:59:53 +0200

    fixed UI issue in posting comments, also fixed manifest

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  process.env.API_URL || "https://scifi-amsterdam.herokuapp.com/";
+  process.env.API_URL || "https://scifi-amsterdam.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;

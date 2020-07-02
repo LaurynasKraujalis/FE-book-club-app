f1ab08f7bbab61a8e5d2fc123126e93a688a2c75
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-07-02 15:46:10 +0200

    changed navigation to a dropdown button

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,60 +5,55 @@ import NavbarItem from "./NavbarItem";
 import { logOut } from "../../store/user/actions";
 import { selectToken } from "../../store/user/selectors";
 
-import { Navbar, Nav } from "react-bootstrap";
+import "./index.css";
+
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
 export default function Navigation() {
-  const [expanded, setExpanded] = useState(false);
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
 
   return (
-    <Navbar
-      collapseOnSelect
-      bg="danger"
-      expand="lg"
-      sticky="top"
-      expanded={expanded}
-    >
-      <Navbar.Toggle
-        aria-controls="responsive-navbar-nav"
-        onClick={() => setExpanded(expanded ? false : true)}
-      />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav style={{ width: "100%" }} fill>
-          <NavbarItem path="/" linkText="Home" />
-          <NavbarItem path="/members" linkText="Members" />
-          {token ? (
-            <NavbarItem
-              path="/newbook"
-              linkText="Post new book"
-              onClick={() => setExpanded(false)}
-            />
-          ) : null}
+    <main>
+      <nav className="navbar">
+        <Dropdown>
+          <Dropdown.Toggle variant="danger" id="dropdown-basic">
+            Menu
+          </Dropdown.Toggle>
 
-          {token ? null : (
-            <NavbarItem
-              path="/login"
-              linkText="Log in"
-              onClick={() => setExpanded(false)}
-            />
-          )}
-          {token ? (
-            <NavbarItem
-              path="/myprofile"
-              linkText="My profile"
-              onClick={() => setExpanded(false)}
-            />
-          ) : null}
+          <Dropdown.Menu className="dropdown-menu">
+            <Dropdown.Item>
+              <NavbarItem path="/" linkText="Home" />
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <NavbarItem path="/members" linkText="Members" />
+            </Dropdown.Item>
+            <Dropdown.Item>
+              {token ? (
+                <NavbarItem path="/newbook" linkText="Post new book" />
+              ) : null}
+            </Dropdown.Item>
+            <Dropdown.Item>
+              {token ? null : <NavbarItem path="/login" linkText="Log in" />}
+            </Dropdown.Item>
+            <Dropdown.Item>
+              {token ? (
+                <NavbarItem path="/myprofile" linkText="My profile" />
+              ) : null}
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
 
+        <div>
           {token ? (
             <Button variant="danger" onClick={() => dispatch(logOut())}>
               Logout
             </Button>
           ) : null}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+        </div>
+      </nav>
+    </main>
   );
 }
